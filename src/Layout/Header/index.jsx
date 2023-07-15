7a67371f90c295f451b2b3d6d5195ed16b50437a
Ibnul Ashir <ibnulashir42@gmail.com>
2023-07-16 02:39:05 +0600

    weight section

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -18,13 +18,13 @@ const Header = ({ open, setOpen }) => {
   return (
     <div className='bg-gradient-to-r from-[#84BFB5] to-[#A6CBC5] text-black'>
       <div className="max-w-7xl mx-auto px-2 py-2 flex flex-col lg:flex-row justify-between items-center">
-        <div className="flex w-full lg:w-auto justify-between">
+        <div className="flex w-full lg:w-auto justify-between items-center">
           <div className="">
-            <Link to="/"><img src="/assets/logo.png" className='w-2/5' alt="logo" /></Link>
+            <Link to="/"><img src="/assets/logo.png" className='w-8/12' alt="logo" /></Link>
           </div>
 
           <div
-            className={`h-12 w-12 flex flex-col justify-between items-center rounded cursor-pointer md:hidden border ${open ? "p-2" : "p-3"
+            className={`h-12 w-12 flex flex-col justify-between items-center rounded cursor-pointer lg:hidden border ${open ? "p-2" : "p-3"
               } z-50`}
             onClick={() => setOpen(!open)}
           >
@@ -44,15 +44,15 @@ const Header = ({ open, setOpen }) => {
         </div>
         <div className={`flex flex-col gap-3 lg:flex-row items-start lg:items-center pt-10 lg:pt-0 justify-start lg:justify-center absolute left-0 lg:static w-full lg:h-auto lg:w-auto transition-all duration-500 ease-in ${
             open
-              ? "opacity-100 top-[65px] bg-gradient-to-r from-[#84BFB5] to-[#A6CBC5] lg:bg-transparent h-[calc(100vh-65px)]"
-              : "opacity-0 lg:opacity-100 top-[65px] left-[-500px]"
+              ? "opacity-100 top-[75px] bg-gradient-to-r from-[#84BFB5] to-[#A6CBC5] lg:bg-transparent h-[calc(100vh-75px)]"
+              : "opacity-0 lg:opacity-100 top-[75px] left-[-500px]"
           } z-50`}>
           <ul className='flex flex-col lg:flex-row gap-6'>
             {links.map((link, i) => (
               <NavLink style={navLinkStyles} key={i} to={link.link} className='capitalize hover:text-white px-3 lg:px-0'>{link.name}</NavLink>
             ))}
           </ul>
-          <Link to="/" className='bg-[#FFDE17] px-5 py-3 rounded'>Patient Portal</Link>
+          <Link to="/" className='bg-[#FFDE17] px-5 mx-3 py-3 rounded font-bold'>Patient Portal</Link>
         </div>
       </div>
     </div>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -40,6 +40,30 @@ const services = [
   }
 ]
 
+const steps = [
+  {
+    title: 'Step 1',
+    color: '#7C3EDA',
+    name: 'Medical History',
+    source: '/assets/Star2.png',
+    desc: "Complete a simple medical history form so your provider can develop the best treatment plan for you. It's fast, easy, and free."
+  },
+  {
+    title: 'Step 2',
+    color: '#17CBBE',
+    name: 'Online Visit',
+    source: '/assets/Star3.png',
+    desc: 'Speak to your new healthcare provider licensed to treat your condition with a simple online audio-visual appointment conversation.'
+  },
+  {
+    title: 'Step 3',
+    color: '#FF3755',
+    name: 'Receive Medications',
+    source: '/assets/Star4.png',
+    desc: 'If medically appropriate, receive your medication every 30/60/90 days. All GLP-1 Plans come with free shipping directly to your door! Follow up with your provider any time you need assistance.'
+  }
+]
+
 const Home = () => {
   return (
     <div>
@@ -75,7 +99,7 @@ const Home = () => {
             <p className='text-3xl md:stext-5xl font-medium'>Our Services</p>
             <div className="bg-[#FFDE17] w-24 h-1 rounded"></div>
           </div>
-          <div className="flex flex-wrap justify-center md:justify-between gap-5">
+          <div className="flex flex-wrap justify-center gap-5">
             {services && services.map((service, index) => (
               <div key={index} className="min-w-[250px] max-w-[280px] h-[310px] rounded-lg shadow-lg hover:shadow-2xl border flex flex-col gap-2 items-center justify-between px-2 text-center my-10">
                 <img src={service.source} className='w-24 h-24 -mt-12' alt={service.title} />
@@ -84,23 +108,71 @@ const Home = () => {
                   <div className="bg-[#FFDE17] w-24 h-[2px] rounded"></div>
                 </div>
                 <p className='text-[#969696] my-3'>{service.desc}</p>
-                <button className='bg-[#FFDE17] py-2 px-6 rounded -mb-5'>View All</button>
+                <button className='bg-[#FFDE17] py-2 px-6 rounded -mb-5 font-extrabold'>View All</button>
               </div>
             ))}
           </div>
-          <div className="w-full bg-[#6CB4A8] py-7 px-6 flex flex-col gap-3 md:flex-row justify-between items-center rounded-md text-white">
+          <div className="w-full bg-[#6CB4A8] py-7 px-6 flex flex-col gap-3 md:flex-row justify-between md:gap-10 md:justify-center items-center rounded-md text-white">
             <div className="text-3xl md:text-4xl font-medium text-center md:px-5">
               <p>Non-weight related</p>
               <p>telehealth visits!</p>
             </div>
             <div className="flex flex-col lg:flex-row justify-end gap-3 md:gap-6">
-              <button className='bg-[rgba(163,189,49,0.7)] hover:bg-[rgba(163,189,49,1)] border-4 border-white flex flex-col items-center py-4 md:px-10 rounded-md text-4xl font-medium text-center min-w-[280px]'><span>$99</span><span>New patient</span></button>
-              <button className='bg-[rgba(219,150,0,0.7)] hover:bg-[rgba(219,150,0,1)] border-4 border-white flex flex-col items-center py-4 md:px-10 rounded-md text-4xl font-medium text-center min-w-[280px]'><span>$99</span><span>New patient</span></button>
+              <button className='bg-[rgba(163,189,49,1)] border-4 border-white flex flex-col items-center py-4 md:px-10 rounded-md text-4xl font-medium text-center min-w-[280px]'><span>$99</span><span>New Patient</span></button>
+              <button className='bg-[rgba(219,150,0,1)] border-4 border-white flex flex-col items-center py-4 md:px-10 rounded-md text-4xl font-medium text-center min-w-[280px]'><span>$80</span><span>Follow-up</span></button>
             </div>
           </div>
         </div>
       </section>
       {/* ------------------  service section ------------------ */}
+      {/* ------------------  weightloss section ------------------ */}
+      <section className="w-full relative overflow-hidden bg-[url('/assets/weightloss_bg.png')] bg-cover bg-center bg-repeat py-10 space-y-5">
+        <div className="w-full flex flex-col gap-2 items-center text-center my-8">
+          <p className='text-3xl md:stext-5xl font-medium'>Weight Loss Subscription</p>
+          <div className="bg-[#FFDE17] w-24 h-1 rounded"></div>
+        </div>
+        <p className='text-[#666666] font-semibold text-xl text-center'>How Weight Loss Subscription Works</p>
+        <div className="flex justify-center">
+          <p className='text-lg text-center text-[#666666] max-w-2xl'>Bee Well Health offers healthcare subscription services to our clients, providing access to high-quality practitioners for a fixed monthly price, no insurance needed, and no hidden fees. Cancel anytime! </p>
+        </div>
+        <div className="py-5 md:py-7 lg:py-20 flex flex-wrap gap-8 justify-center">
+          {steps && steps.map((step, index) => (
+            <div key={index} className="relative">
+              <div className="w-[315px] md:w-[330px] lg:w-[350px] 2xl:w-[380px]">
+                <img src={step.source} className='w-full' alt="star" />
+              </div>
+              <div className="absolute top-0 w-[315px] md:w-[330px] lg:w-[350px] 2xl:w-[380px] h-full space-y-2 px-6 py-5 text-center">
+                <p className={`font-bold ${index === 0 && 'text-[#7C3EDA]'} ${index === 1 && 'text-[#17CBBE]'}  ${index === 2 && 'text-[#FF3755]'} text-4xl`}>{step.title}</p>
+                <p className='font-medium text-2xl'>{step.name}</p>
+                <p className='text-[#666666] text-sm md:text-base'>{step.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="w-full flex flex-col gap-2 items-center text-center my-8">
+          <p className='text-3xl md:stext-5xl font-medium'>Weight Loss Plans</p>
+          <div className="bg-[#FFDE17] w-24 h-1 rounded"></div>
+        </div>
+        <div className="flex flex-wrap justify-center gap-5 md:gap-8 lg:gap-10">
+          <div className="bg-[url('/assets/weightloss1.png')] bg-cover bg-center min-w-[280px] max-w-[350px] overflow-hidden rounded-lg py-8 px-5 md:px-8 flex flex-col gap-4 items-center">
+            <p className='text-xl text-white font-bold'>GLP-1 Weight Management</p>
+            <div className="w-full h-[1px] bg-white"></div>
+            <p className='text-3xl text-white font-bold'>$280/month</p>
+            <div className="py-10 mt-5">
+              <p className=''>Includes personalized telehealth appointments with provider & medication + supplies <span className='text-[#14B0B0]'>shipped to your door</span></p>
+            </div>
+          </div>
+          <div className="bg-[url('/assets/weightloss2.png')] bg-cover bg-center min-w-[280px] max-w-[350px] overflow-hidden rounded-lg py-8 px-5 md:px-8 flex flex-col gap-4 items-center">
+            <p className='text-xl text-white font-bold'>Medical Weight Loss</p>
+            <div className="w-full h-[1px] bg-white"></div>
+            <p className='text-3xl text-white font-bold'>$80/month</p>
+            <div className="py-10 mt-5">
+              <p className=''>Includes personalized telehealth appointments with provider & electronically sent prescription <span className='text-[#FD9177]'>to the pharmacy of your choice</span></p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ------------------  weightloss section ------------------ */}
     </div>
   )
 }
